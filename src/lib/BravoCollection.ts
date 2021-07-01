8b1883c4cf178522c47dff8aba8133bc511a0477
Adam Coster <adam@bscotch.net>
2021-07-01 18:26:08 -0500

    feat: Add partial methods to the Collection class to allow instances to find their own Widgets.

--- src/lib/BravoCollection.ts
+++ src/lib/BravoCollection.ts
@@ -1,5 +1,6 @@
 import { DataFavroCollection } from '../types/FavroApiTypes';
 import { BravoEntity } from './BravoEntity.js';
+import { BravoWidget } from './BravoWidget.js';
 
 export class BravoCollection extends BravoEntity<DataFavroCollection> {
   get name() {
@@ -12,6 +13,40 @@ export class BravoCollection extends BravoEntity<DataFavroCollection> {
 
   get organizationId() {
     return this._data.organizationId;
+  }
+
+  /**
+   * Get list of all widgets in this collection
+   */
+  async listWidgets() {
+    return await this._client.listWidgets(this.collectionId);
+  }
+
+  /**
+   * Find the first widget in this collection for which the
+   * `matchFunction` returns a truthy value. Can be async
+   */
+  async findWidget(
+    matchFunction: (widget: BravoWidget, idx?: number) => any | Promise<any>,
+  ) {
+    return this._client.findWidget(matchFunction, this.collectionId);
+  }
+
+  /**
+   * Find the first widget in this collection that matches
+   * the provided name. Ignores case by default.
+   */
+  async findWidgetByName(
+    name: string,
+    options?: {
+      matchCase?: boolean;
+    },
+  ) {
+    return await this._client.findWidgetByName(
+      name,
+      this.collectionId,
+      options,
+    );
   }
 
   /** Delete this collection from Favro. **Use with care!** */
