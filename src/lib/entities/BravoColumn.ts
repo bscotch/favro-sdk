b2318a760b44b753d75f01e0c0741e827a080bad
Adam Coster <adam@bscotch.net>
2021-07-02 08:16:28 -0500

    feat: Draft the Column class.

---
+++ src/lib/entities/BravoColumn.ts
@@ -0,0 +1,32 @@
+import { BravoEntity } from '$lib/BravoEntity.js';
+import { DataFavroColumn } from '$/types/FavroColumnTypes.js';
+
+export class BravoColumn extends BravoEntity<DataFavroColumn> {
+  get columnId() {
+    return this._data.columnId;
+  }
+  get widgetCommonId() {
+    return this._data.widgetCommonId;
+  }
+  get name() {
+    return this._data.name;
+  }
+  get organizationId() {
+    return this._data.organizationId;
+  }
+  get position() {
+    return this._data.position;
+  }
+  get cardCount() {
+    return this._data.cardCount;
+  }
+  get timeSum() {
+    return this._data.timeSum;
+  }
+  get estimationSum() {
+    return this._data.estimationSum;
+  }
+  equals(column: BravoColumn) {
+    return this.hasSameConstructor(column) && this.columnId === column.columnId;
+  }
+}
