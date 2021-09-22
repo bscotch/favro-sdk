ab834c18fa25de84f0fc6f1eb03b2da1f978b135
Adam Coster <adam@bscotch.net>
2021-09-22 16:50:35 -0500

    feat: Add export of BravoEntity types in the entryfile, so that they're more accessible.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/BravoClient.js';
 export * from './types/FavroApi.js';
+
+export * as BravoEntities from './types/Bravo.js';
---
+++ src/types/Bravo.ts
@@ -0,0 +1,8 @@
+export type { BravoCardInstance } from '$lib/entities/BravoCard';
+export type { BravoCollection } from '$lib/entities/BravoCollection';
+export type { BravoColumn } from '$lib/entities/BravoColumn';
+export type { BravoCustomField } from '$lib/entities/BravoCustomField';
+export type { BravoOrganization } from '$lib/entities/BravoOrganization';
+export type { BravoTagDefinition } from '$lib/entities/BravoTag';
+export type { BravoUser } from '$lib/entities/BravoUser';
+export type { BravoWidget } from '$lib/entities/BravoWidget';
